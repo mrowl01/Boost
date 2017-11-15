6d8d02f1de338beb213a595fbd4dd11fab0eb926
Chris <christopherortega56@gmail.com>
2017-11-15 00:55:40 -0600

    Made a rocket and added rigidbody so it reacts to input via crossplatform

---
+++ Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Rocket : MonoBehaviour {
+
+	public float thrustForce= 25;
+
+	private Vector3 thrust;
+	private Vector3 rotation;
+	private Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput ();
+	}
+
+	 void ProcessInput ()
+	{
+		thrust.y = CrossPlatformInputManager.GetAxis ("Jump") * thrustForce;
+		thrust.x = 0;
+		thrust.z = 0;
+
+		rotation.y = 0;
+		rotation.z= CrossPlatformInputManager.GetAxis("Horizontal");
+		rotation.x = 0;
+
+		transform.Rotate (-rotation);
+		rigidBody.AddRelativeForce (thrust);
+
+	}
+}
